ae8923fdaf85f7a6227d52362a9cf700edc8b30c
Nashte <ajinkyareact9@gmail.com>
2026-08-11 20:40:55 +0530

    fix: describe changed to test in hometest component

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,10 +1,10 @@
 // __tests__/home.test.tsx
 // @vitest-environment jsdom
 import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, test } from 'vitest';
 import Home from '../app/page';
 
-describe('Home Page Unit Tests', () => {
+test('Home Page Unit Tests', () => {
   it('renders the correct welcome heading text', () => {
     render(<Home />);
     const heading = screen.getByTestId('main-heading');
